45d2ea9c1583d22e00daf1996e1e53a2f4c3310f
Sabbir-97 <mdsabbirahmmed87@gmail.com>
2021-11-21 20:36:32 +0600

    icon change

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -7,6 +7,10 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-arrow-alt-circle-right "></i></div>
+                                All Waiting User
+                            </a>
                             <a class="nav-link" href="{{route('allBook')}}">
                                 <div class="sb-nav-link-icon"><i class="fas fa-user-secret"></i></div>
                                 All Book
