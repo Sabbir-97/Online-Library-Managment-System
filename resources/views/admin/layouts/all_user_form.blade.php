eecad91812f4f341f9dfe1c172737eb1a3245068
Sabbir-97 <mdsabbirahmmed87@gmail.com>
2021-11-27 22:28:14 +0600

    form create

--- resources/views/admin/layouts/all_user_form.blade.php
+++ resources/views/admin/layouts/all_user_form.blade.php
@@ -5,41 +5,81 @@
 </br>
 <form>
     
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter your User ID</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter Your First Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter Your Last Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter Your Email Address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Enter Your Contact Number</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Religion</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Gender</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
+<div class="card-content">
+        <form Action="{{route('bookForm')}}" method="post" enctype="multipart/form-data">
+          @csrf
+          
+          <div class="field">
+            <label class="label">User Id</label>
+            <div class="control">
+              <input required class="input" name="book_title" type="text" placeholder="Enter your user id">
+            </div>
+          </div>
 
+          <div class="field">
+            <label class="label">Fast Name</label>
+            <div class="control">
+              <input required class="input" name="category" type="text" placeholder="Enter your first name">
+            </div>
+          </div>
 
-  
-  <!-- <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div> -->
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+          <div class="field">
+            <label class="label">Last Name</label>
+            <div class="control">
+              <input required class="input" name="available_books" type="text" placeholder="Enter your last name ">
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Email Adress</label>
+            <div class="control">
+              <input required class="input" name="available_books" type="text" placeholder="Enter your email adress ">
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input required class="input" name="available_books" type="text" placeholder="Enter your password ">
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Image :</label>
+            <!-- <div class="control"> -->
+              <input required class="input" name="image_path" type="file" placeholder="Choose your Image ">
+            <!-- </div> -->
+          </div>
+
+          <div class="field">
+            <label class="label">Gender :</label>
+            <!-- <div class="control"> -->
+            <input type="radio" name="gender" id="gender" value="male"> Male
+					  <input type="radio" name="gender" id="gender" value="female"> Female
+					  <input type="radio" name="gender" id="gender" value="others"> Others
+            <!-- </div> -->
+          </div>
+
+          <div class="field">
+            <label class="label">Religion :</label>
+            <!-- <div class="control"> -->
+            <input type="radio" name="religion" id="religion" value="islam"> Islam
+					  <input type="radio" name="religion" id="religion" value="hindu"> Hindu
+					  <input type="radio" name="religion" id="religion" value="others"> Others
+
+            <!-- <input type="checkbox" name="c1" id="religion" value="islam"> Islam
+					  <input type="checkbox" name="c1" id="religion" value="hindu"> Hindu
+					  <input type="checkbox" name="c1" id="religion" value="others"> Others -->
+            <!-- </div> -->
+          </div>
+
+          <div class="field grouped">
+            <div class="control">
+              <button type="submit" class="submit-button">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
 @endsection
